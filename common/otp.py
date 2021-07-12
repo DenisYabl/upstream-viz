3281b43a03eb630ae6e30adb08bd6d4a476ad268
Nikolay Ryabykh <nryabykh@isgneuro.com>
2021-07-12 03:39:36 +0300

    move ttl to params, add types

--- common/otp.py
+++ common/otp.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 @st.cache
-def get_data(query):
+def get_data(query: str, ttl=60) -> pd.DataFrame:
     conn = config.get_rest_connector()
-    df = pd.DataFrame(conn.jobs.create(query, cache_ttl=60, tws=0, twf=0).dataset.load())
+    df = pd.DataFrame(conn.jobs.create(query, cache_ttl=ttl, tws=0, twf=0).dataset.load())
     if "_time" in df.columns:
         df["dt"] = pd.to_datetime(df["_time"], unit="s")
     return df
